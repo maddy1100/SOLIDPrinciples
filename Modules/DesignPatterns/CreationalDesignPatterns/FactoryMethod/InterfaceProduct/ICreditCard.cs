982dbc8103817ffe409eb6adb186852e8240146e
Madan Reddy <madanmohan705@gmail.com>
2024-04-20 18:16:16 +0530

    feat: Design Patterns - Factory Method Implemented

---
+++ Modules/DesignPatterns/CreationalDesignPatterns/FactoryMethod/InterfaceProduct/ICreditCard.cs
@@ -0,0 +1,11 @@
+﻿namespace BestPractices.Modules.DesignPatterns.CreationalDesignPatterns.FactoryMethod
+{
+    // The ICreditCard interface declares the operations that all 
+    // concrete products must implement.
+    public interface ICreditCard
+    {
+        string GetCardType();
+        int GetCreditLimit();
+        int GetAnnualCharge();
+    }
+}
